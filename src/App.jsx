201d5612cb771c80a0d909a9b047489b82918e7e
Robin667 <robin.turpin8@gmail.com>
2024-06-05 11:37:43 +0200

    add template to repo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@material-tailwind/react";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<h1 className="text-3xl font-bold">Hello world!</h1>
+			<Button>Click me</Button>
+		</>
+	);
+}
+
+export default App;
