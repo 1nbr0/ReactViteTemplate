7c4ae10181c44b523f884b0b26cbbafb18a275ba
rturpin <rturpin@asi.fr>
2025-04-28 16:32:03 +0200

    Refactor code structure for improved readability and maintainability

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { Button } from "@material-tailwind/react";
 import "./App.css";
 
 function App() {
-	return (
-		<>
-			<h1 className="text-3xl font-bold">Hello world!</h1>
-			<Button>Click me</Button>
-		</>
-	);
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <Button>Click me</Button>
+    </>
+  );
 }
 
 export default App;
